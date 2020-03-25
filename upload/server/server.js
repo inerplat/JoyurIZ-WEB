b1c5160e258ecda6bbca21016d845006475dcbfc
DH Kim <inerplat@gmail.com>
2020-03-25 13:01:01 +0900

    Check md5 hash from db and get prediction to post flask server

--- upload/server/server.js
+++ upload/server/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const request = require('request')
 const fs = require('fs')
 const multer = require('multer')
@@ -5,76 +6,127 @@ const express = require('express')
 const app = express()
 const uuid4 = require('uuid4')
 const path = require('path');
+const md5File = require('md5-file/promise')
+const mongoose = require('mongoose')
+const schema = require('./mongoImage.js') 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-
 const uuid = () => {
     const tokens = uuid4().split('-')
     return tokens[2] + tokens[1] + tokens[0] + tokens[3] + tokens[4]
 }
+
+mongoose.Promise = global.Promise;
+mongoose.set('useFindAndModify', true);
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true,  useUnifiedTopology: true })
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch(e => console.error(e));
+
+
 var storage = multer.diskStorage({
-    destination : function(req, file, cb){    
-      cb(null, 'userUpload/');
-    },
-    filename : function(req, file, cb){
-      var mimeType;
-      switch (file.mimetype) {
-        case "image/jpeg":
-          mimeType = "jpg";
-        break;
-        case "image/png":
-          mimeType = "png";
-        break;
-        case "image/gif":
-          mimeType = "gif";
-        break;
-        case "image/bmp":
-          mimeType = "bmp";
-        break;
-        default:
-          mimeType = "null";
-        break;
-      }
-      cb(null, uuid() + "." + mimeType);
+  destination : function(req, file, cb){    
+    cb(null, 'userUpload/');
+  },
+  filename : function(req, file, cb){
+    var mimeType;
+    switch (file.mimetype) {
+      case "image/jpeg":
+        mimeType = "jpg";
+      break;
+      case "image/png":
+        mimeType = "png";
+      break;
+      case "image/gif":
+        mimeType = "gif";
+      break;
+      case "image/bmp":
+        mimeType = "bmp";
+      break;
+      default:
+        mimeType = "null";
+      break;
     }
-  });
-  
-  var upload = multer({
-        storage: storage,
-        fileFilter: function (req, file, callback) {
-            var ext = path.extname(file.originalname);
-            if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
-                return callback(new Error('Only images are allowed'))
-            }
-            callback(null, true)
-        },
-        limits:{
-            fileSize: 1024 * 1024 * 10
-        }
+    cb(null, uuid() + "." + mimeType);
+  }
 });
 
+var upload = multer({
+      storage: storage,
+      fileFilter: function (req, file, callback) {
+          var ext = path.extname(file.originalname);
+          if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+              return callback(new Error('Only images are allowed'))
+          }
+          callback(null, true)
+      },
+      limits:{
+          fileSize: 1024 * 1024 * 10
+      }
+});
+const Image = mongoose.model('Image', schema.imageSchema);
+
 app.post('/' , upload.single('image'), (req, res)=>{
-    type = req.file.mimetype.split('/')
+  fileName = req.file['filename']
+  console.log(fileName)
+  md5File('userUpload/'+fileName)
+  .then(hash => {
+    Image.find({hash:hash},(err,res)=>{
+      console.log(res);
+      if(res.length===0){
+        doRequest('http://127.0.0.1:5000/predict', 5000, 'userUpload/'+fileName)
+        .then(result=>{
+          console.log(result, result["top"])
+          Image.create({
+            hash:         hash,
+            predictions:   result["predictions"],
+            top:          result["top"],
+            bottom:       result["bottom"],
+            left:         result["left"],
+            right:        result["right"],
+            path:         'userUpload/' + fileName
+          })
+        })
+        
+      }
+      else{
+        console.log("delete")
+        fs.unlink('userUpload/'+fileName, (err) => {
+          if (err) {
+            console.error(err)
+          }
+       })
+      }
+    
+    });
+  })
 })
+
 var port = 8080
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
 //    doRequest()
 })
 
-var files = {
-    image: fs.createReadStream('Yuri562.png')
-}
-function doRequest(url) {
-    return new Promise((resolve, reject)=>{
-        request.post({ url: 'http://127.0.0.1:5000/predict', formData: files}, (error, res, body)=>{
-            console.log(res)
-            console.log(body)
-        })
-    });
-    
+
+function doRequest(url, port, filePath) {
+  return new Promise((resolve, reject)=>{
+      var options = {
+          method: "POST",
+          url: url, 
+          port: port,
+          headers: {
+              "Content-Type": "multipart/form-data"
+          },
+          formData : {
+              "image" : fs.createReadStream(filePath)
+          }
+      };
+      request.post(options, (error, res, body)=>{
+        resolve(JSON.parse(body))
+      })
+  }); 
 }
