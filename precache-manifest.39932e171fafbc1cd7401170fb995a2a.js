14e9e4d5c61848383bd7bc9217e407b3ccaa5574
DH Kim <inerplat@gmail.com>
2020-04-04 00:44:37 +0900

    Updates

---
+++ precache-manifest.39932e171fafbc1cd7401170fb995a2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b25c9dabe12a1aed6640f093d6b5b962",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0bc4130f9320e05519a",
+    "url": "/static/css/main.19e3e23a.chunk.css"
+  },
+  {
+    "revision": "21f19ebe9aaf10ecd2c5",
+    "url": "/static/js/2.254b2223.chunk.js"
+  },
+  {
+    "revision": "b948d2a7727b83687a00ec53ef5267c0",
+    "url": "/static/js/2.254b2223.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0bc4130f9320e05519a",
+    "url": "/static/js/main.a93bb558.chunk.js"
+  },
+  {
+    "revision": "0698687af6d5316a5c60",
+    "url": "/static/js/runtime-main.3847e76f.js"
+  }
+]);
