9ed7568e5cd7b827c7397f74e39c46642364d4c6
DH Kim <inerplat@gmail.com>
2021-09-21 06:39:24 +0900

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -5,4 +5,4 @@ import sys
 if __name__ == '__main__':
   handler = logging.StreamHandler(sys.stdout)
   app.logger.addHandler(handler)
-  app.run(debug=False, host='0.0.0.0')
+  app.run(debug=False, host='0.0.0.0', port=80)
