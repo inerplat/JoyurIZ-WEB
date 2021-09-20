893578baeed5befc7151fba978cc24f1a925f7b2
DH Kim <inerplat@gmail.com>
2021-09-20 13:33:23 +0000

    make flask docker base image

--- flask/app.py
+++ flask/app.py
@@ -2,7 +2,6 @@ from joyuriz import app
 import logging
 import sys
 
-if __name__ == '__main__':
-    handler = logging.StreamHandler(sys.stdout)
-    app.logger.addHandler(handler)
-    app.run(debug=False, host='0.0.0.0')
+handler = logging.StreamHandler(sys.stdout)
+app.logger.addHandler(handler)
+app.run(debug=False, host='0.0.0.0')
