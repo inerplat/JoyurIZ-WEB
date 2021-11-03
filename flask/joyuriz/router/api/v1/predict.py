83dcb5283f733501f6c30e8ccfec0a820ad580ae
DH Kim <inerplat@gmail.com>
2021-11-03 15:25:01 +0900

    Update predict.py

--- flask/joyuriz/router/api/v1/predict.py
+++ flask/joyuriz/router/api/v1/predict.py
@@ -3,6 +3,7 @@ from flask_restx import Namespace, Resource
 from joyuriz.service.image import ImageManager
 from joyuriz.service.model import ModelManager
 from joyuriz.service.cnn import CNN
+import base64
 predict_ns = Namespace('Predict', path='/api/v1', description='predict')
 
 swagger_parser = predict_ns.parser()
@@ -24,8 +25,10 @@ class Predict(Resource):
     def post(self):
         if "image" not in request.files:
             raise ValueError
-
+        
         input_image = request.files["image"].read()
+        if("Content-Transfer-Encoding" in request.headers and request.headers["Content-Transfer-Encoding"] == "base64"):
+            input_image = base64.decodebytes(input_image)
         manager = ImageManager(input_image)
 
         tensor_image = manager.find_face().crop_image().to_tensor().get_image()
